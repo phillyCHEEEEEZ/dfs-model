26a067a340d3fdfaf538c6ac71eebdb29d3be7b9
pnacamuli17 <panac@nacamuli.net>
2019-10-02 10:01:00 -0500

    tidy up code

--- fantasycruncher nhl.py
+++ fantasycruncher nhl.py
@@ -17,12 +17,11 @@ chromedriver = (
     'C:/dev/Web Drivers/chromedriver_win32/chromedriver.exe')
 
 options = webdriver.ChromeOptions()
-# options.add_argument(
-#    'download.default_directory=C:/dev/Python/Scripts/Web Scraping/')
-# or
+
 prefs = {'download.default_directory': 'C:\dev\Python\Scripts\Web Scraping\\',
          'download.prompt_for_download': False,
          'download.directory_upgrade': True, }
+
 options.add_experimental_option('prefs', prefs)
 
 driver = webdriver.Chrome(chromedriver, options=options)
@@ -56,27 +55,3 @@ downloadPlayerlist.click()
 time.sleep(5)
 
 driver.close()
-
-# ==========================================================
-# ==========================================================
-# ==========================================================
-# DFS = []
-# calendar = []
-# calendar.append('2018-10-30')
-# calendar.append('2018-10-31')
-# for d in calendar:
-# driver.get('https://www.fantasycruncher.com/lineup-cruncher/fanduel/NHL')
-# closeButton = driver.find_element_by_class_name('close-login-alert')
-# closeButton.click()
-# select = Select(driver.find_element_by_name('ff_length'))
-# select.select_by_value('-1')
-# actions = driver.find_element_by_id('table-actions')
-# actions.click()
-# WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-#    (By.XPATH, '//div[@data-action='downloadPlayerlist']')))
-# downloadPlayerlist = driver.find_element_by_xpath(
-#    '//div[@data-action='downloadPlayerlist']')
-# downloadPlayerlist.click()
-
-# remove the comment below to close the browser
-# driver.close()
