7f7eb7606c6a482acd15d1772451770207b015db
pnacamuli17 <panac@nacamuli.net>
2019-10-11 16:07:37 -0500

    update archive script

--- nhl/scripts/python/archive/archive and append timestamps.py
+++ nhl/scripts/python/archive/archive and append timestamps.py
@@ -7,15 +7,24 @@ now = datetime.datetime.now()
 today = str(now.strftime('%Y-%m-%d'))
 timestamp = str(now.strftime('_%Y-%m-%d_%H-%M-%S'))
 
+# datetime adjust
+# now = now - datetime.timedelta(days=1)
+
 # working directory and archive directory
 working_dir = 'c:/dev/Python/Repos/dfs-model/nhl/data/'
 archive_dir = 'c:/dev/Python/Repos/dfs-model/nhl/data/archive/'
 
+# folders
+fc_folder = 'fantasy cruncher/'
+rw_folder = 'rotowire/'
+nf_folder = 'numberfire/'
+agg_folder = 'aggregate/'
+
 
 # archive function
-def appendTimestampAndArchive(working_dir, archive_dir, filename, extension, timestamp):
+def appendTimestampAndArchive(working_dir, archive_dir, folder, filename, extension, timestamp):
     src = working_dir + filename + '.' + extension
-    dst = archive_dir + filename + timestamp + '.' + extension
+    dst = archive_dir + folder + filename + timestamp + '.' + extension
     shutil.copy(src, dst)
     os.remove(src)
 
@@ -23,17 +32,29 @@ def appendTimestampAndArchive(working_dir, archive_dir, filename, extension, tim
 # fantasycruncher
 filename = 'fanduel_NHL_' + today + '_players'
 extension = 'csv'
-appendTimestampAndArchive(working_dir, archive_dir,
+appendTimestampAndArchive(working_dir, archive_dir, fc_folder,
                           filename, extension, timestamp)
 
 # rotowire
 filename = 'rotowire-fanduel-NHL-players'
 extension = 'csv'
-appendTimestampAndArchive(working_dir, archive_dir,
+appendTimestampAndArchive(working_dir, archive_dir, rw_folder,
                           filename, extension, timestamp)
 
 # numberfire
 filename = 'numberfire_fanduel_all'
 extension = 'csv'
-appendTimestampAndArchive(working_dir, archive_dir,
+appendTimestampAndArchive(working_dir, archive_dir, nf_folder,
+                          filename, extension, timestamp)
+
+# aggregate
+filename = 'aggregate_projections'
+extension = 'csv'
+appendTimestampAndArchive(working_dir, archive_dir, agg_folder,
+                          filename, extension, timestamp)
+
+# fc upload
+filename = 'fc_upload'
+extension = 'csv'
+appendTimestampAndArchive(working_dir, archive_dir, fc_folder,
                           filename, extension, timestamp)
