b0d5d72b5526181c09a29e0b32807db9e84697a0
pnacamuli17 <panac@nacamuli.net>
2019-10-13 16:53:21 -0500

    add timeout

--- nhl/scripts/batch/01 - Scrape Projections.bat
+++ nhl/scripts/batch/01 - Scrape Projections.bat
@@ -4,5 +4,6 @@ timeout 5
 python "c:\dev\Python\Repos\dfs-model\nhl\scripts\python\projections\rotowire nhl.py"
 timeout 5
 python "c:\dev\Python\Repos\dfs-model\nhl\scripts\python\projections\numberfire nhl.py"
+timeout 5
 exit
 
