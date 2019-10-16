cfd4da80a16607923b801ae2288496bb8779bb06
pnacamuli17 <panac@nacamuli.net>
2019-10-16 11:36:06 -0500

    include daily fantasy fuel data in aggregation

--- nhl/scripts/python/model/aggregate nhl.py
+++ nhl/scripts/python/model/aggregate nhl.py
@@ -15,12 +15,14 @@ names_filename = 'static/names.xlsx'
 fc_filename = 'fanduel_NHL_' + today + '_players.csv'
 rw_filename = 'rotowire-fanduel-NHL-players.csv'
 nf_filename = 'numberfire_fanduel_all.csv'
+dff_filename = 'dff_fanduel_all.csv'
 
 # read CSVs
 names_df = pd.read_excel(data_dir + names_filename)
 fc_df = pd.read_csv(data_dir + fc_filename)
 rw_df = pd.read_csv(data_dir + rw_filename)
 nf_df = pd.read_csv(data_dir + nf_filename)
+dff_df = pd.read_csv(data_dir + dff_filename)
 
 # create aggregate data frame
 agg_df = fc_df
@@ -47,13 +49,14 @@ agg_df['Opponent'] = agg_df['Opponent'].replace(regex=['SJ'], value='SJS')
 agg_df['Opponent'] = agg_df['Opponent'].replace(regex=['TB'], value='TBL')
 
 # merge projections
-agg_df = agg_df.merge(names_df[['FC', 'RW', 'NF']],
+agg_df = agg_df.merge(names_df[['FC', 'RW', 'NF', 'DFF']],
                       left_on='Player', right_on='FC', how='left')
 
 del agg_df['FC_y']
 
 agg_df.rename(columns={'FC_x': 'FC'}, inplace=True)
 
+# rotowire
 agg_df = agg_df.merge(rw_df[['PLAYER', 'FPTS']],
                       left_on='Player', right_on='PLAYER', how='left')
 
@@ -62,6 +65,7 @@ agg_df['RW'] = agg_df['FPTS']
 del agg_df['PLAYER']
 del agg_df['FPTS']
 
+# numberfire
 agg_df = agg_df.merge(nf_df[['Name', 'Projection']],
                       left_on='Player', right_on='Name', how='left')
 
@@ -70,8 +74,17 @@ agg_df['NF'] = agg_df['Projection']
 del agg_df['Name']
 del agg_df['Projection']
 
+# daily fantasy fuel
+agg_df = agg_df.merge(dff_df[['Name', 'Projection']],
+                      left_on='Player', right_on='Name', how='left')
+
+agg_df['DFF'] = agg_df['Projection']
+
+del agg_df['Name']
+del agg_df['Projection']
+
 # average projections
-agg_df['Avg'] = round(agg_df[['FC', 'RW', 'NF']].mean(axis=1), 2)
+agg_df['Avg'] = round(agg_df[['FC', 'RW', 'NF', 'DFF']].mean(axis=1), 2)
 
 # export full CSV and custom FC projections upload CSV
 agg_df.to_csv(
