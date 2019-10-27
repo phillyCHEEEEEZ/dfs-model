6d1449002f096548e5ae9eacd00919b248af9369
pnacamuli17 <panac@nacamuli.net>
2019-10-27 16:17:19 -0500

    update scripts and filenames

---
+++ nhl/scripts/batch/06 - Archive and Append Dates.bat
@@ -0,0 +1,4 @@
+timeout 5
+python "c:\dev\Python\Repos\dfs-model\nhl\scripts\python\archive\archive and append dates.py"
+timeout 5
+exit
--- nhl/scripts/batch/06 - Archive and Append Timestamps.bat
+++
@@ -1,4 +0,0 @@
-timeout 5
-python "c:\dev\Python\Repos\dfs-model\nhl\scripts\python\archive\archive and append timestamps.py"
-timeout 5
-exit
