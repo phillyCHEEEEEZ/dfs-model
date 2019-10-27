e8cacab7eaa243585116e8d8d372f14117edc9f7
pnacamuli17 <panac@nacamuli.net>
2019-10-26 22:06:47 -0500

    update schema

--- nhl/scripts/python/model/aggregate nhl.py
+++ nhl/scripts/python/model/aggregate nhl.py
@@ -57,8 +57,8 @@ del agg_df['FC_y']
 agg_df.rename(columns={'FC_x': 'FC'}, inplace=True)
 
 # rotowire
-agg_df = agg_df.merge(rw_df[['PLAYER', 'FPTS']],
-                      left_on='Player', right_on='PLAYER', how='left')
+agg_df = agg_df.merge(rw_df[['PLAYER', 'FPTS', 'LINE', 'PP', 'ML', 'O/U', 'SPRD', 'TM/P']],
+                      left_on='RW', right_on='PLAYER', how='left')
 
 agg_df['RW'] = agg_df['FPTS']
 
@@ -67,7 +67,7 @@ del agg_df['FPTS']
 
 # numberfire
 agg_df = agg_df.merge(nf_df[['Name', 'Projection']],
-                      left_on='Player', right_on='Name', how='left')
+                      left_on='NF', right_on='Name', how='left')
 
 agg_df['NF'] = agg_df['Projection']
 
@@ -76,15 +76,31 @@ del agg_df['Projection']
 
 # daily fantasy fuel
 agg_df = agg_df.merge(dff_df[['Name', 'Projection']],
-                      left_on='Player', right_on='Name', how='left')
+                      left_on='DFF', right_on='Name', how='left')
 
 agg_df['DFF'] = agg_df['Projection']
 
 del agg_df['Name']
 del agg_df['Projection']
 
+# reorder and rename columns
+agg_df.columns
+
+agg_df = agg_df[['Player', 'Position', 'Team', 'Opponent', 'Salary', 'Date',
+                 'LINE', 'PP', 'ML', 'O/U', 'SPRD', 'TM/P', 'FC', 'RW', 'NF', 'DFF']]
+
+agg_df.columns = ['Player', 'Position', 'Team', 'Opponent', 'Salary', 'Date',
+                  'Line', 'PP Line', 'Moneyline', 'Over/Under', 'Spread', 'Team Pts',
+                  'FC', 'RW', 'NF', 'DFF']
+
 # average projections
 agg_df['Avg'] = round(agg_df[['FC', 'RW', 'NF', 'DFF']].mean(axis=1), 2)
+
+# fix goalie lines/pp lines
+for i in range(0, len(agg_df)):
+    if agg_df['Position'][i] == "G":
+        agg_df['Line'][i] = 0
+        agg_df['PP Line'][i] = 0
 
 # export full CSV and custom FC projections upload CSV
 agg_df.to_csv(
