6c0dc7273502c9211f659009f859765068b6f8de
pnacamuli17 <panac@nacamuli.net>
2019-10-27 16:10:13 -0500

    update deprecated option

--- nhl/scripts/python/model/xgb.py
+++ nhl/scripts/python/model/xgb.py
@@ -55,7 +55,7 @@ y_train = y[:len(df_history)]
 y_test = y[len(df_history):]
 
 # initialize xgboost regressor
-xg_reg = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.3, learning_rate=0.1,
+xg_reg = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.3, learning_rate=0.1,
                           max_depth=5, alpha=10, n_estimators=10)
 
 # fit regressor to training set and make predictions
