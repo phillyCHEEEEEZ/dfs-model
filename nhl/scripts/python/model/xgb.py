96304a01a1be830b913299fa1b5a8ffb26fe837d
pnacamuli17 <panac@nacamuli.net>
2019-10-28 17:21:52 -0500

    update model

--- nhl/scripts/python/model/xgb.py
+++ nhl/scripts/python/model/xgb.py
@@ -19,26 +19,35 @@ wd = 'c:/dev/Python/Repos/dfs-model/'
 df_history = pd.read_csv(wd + '/nhl/data/master/aggregate_projections_all.csv')
 df_today = pd.read_csv(wd + '/nhl/data/aggregate_projections.csv')
 
-# combine data
-df = df_history.append(df_today, ignore_index=True)
+# duplicate data frames
+df = df_history
+df_preds = df_today
 
 # categorical columns
 cat_columns = ['Position']
 
 # one hot encoding
 df = pd.get_dummies(df, prefix_sep="_", columns=cat_columns)
+df_preds = pd.get_dummies(df_preds, prefix_sep="_", columns=cat_columns)
 
 # drop columns
-columns_to_drop = ['Player', 'Team', 'Opponent', 'Salary', 'Date', 'Actual', ]
+columns_to_drop = ['Player', 'Team', 'Opponent', 'Date', 'Avg', ]
 df.drop(columns_to_drop, axis=1, inplace=True)
 
+columns_to_drop = ['Player', 'Team', 'Opponent', 'Date', 'Avg', 'Actual']
+df_preds.drop(columns_to_drop, axis=1, inplace=True)
+
 # reorder
-df = df[['Position_C', 'Position_W', 'Position_D', 'Position_G', 'Line',
-         'PP Line', 'Moneyline', 'Over/Under', 'Spread', 'Team Pts',
-         'FC', 'RW', 'NF', 'DFF', 'Avg']]
+df = df[['Position_C', 'Position_W', 'Position_D', 'Position_G', 'Salary',
+         'Line', 'PP Line', 'Moneyline', 'Over/Under', 'Spread', 'Team Pts',
+         'FC', 'RW', 'NF', 'DFF', 'Actual']]
+
+df_preds = df_preds[['Position_C', 'Position_W', 'Position_D', 'Position_G', 'Salary',
+                     'Line', 'PP Line', 'Moneyline', 'Over/Under', 'Spread', 'Team Pts',
+                     'FC', 'RW', 'NF', 'DFF']]
 
 # drop NAs
-# df.dropna(inplace=True)
+df.dropna(subset=['Actual'], inplace=True)
 
 # separate target variable from rest of variables
 x, y = df.iloc[:, :-1], df.iloc[:, -1]
@@ -49,14 +58,12 @@ data_dmatrix = xgb.DMatrix(data=x, label=y)
 
 ####### basic xgboost #######
 # create train and test sets
-x_train = x[:len(df_history)]
-x_test = x[len(df_history):]
-y_train = y[:len(df_history)]
-y_test = y[len(df_history):]
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=123)
 
 # initialize xgboost regressor
-xg_reg = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.3, learning_rate=0.1,
-                          max_depth=5, alpha=10, n_estimators=10)
+xg_reg = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.3,
+                          learning_rate=0.1, max_depth=5, alpha=10, n_estimators=10)
 
 # fit regressor to training set and make predictions
 xg_reg.fit(x_train, y_train)
@@ -68,7 +75,7 @@ print("RMSE: %f" % (rmse))
 
 # plot feature importance
 xgb.plot_importance(xg_reg)
-plt.rcParams['figure.figsize'] = [5, 5]
+plt.rcParams['figure.figsize'] = [10, 10]
 # plt.show()
 
 
@@ -86,8 +93,11 @@ cv_results.head()
 print((cv_results["test-rmse-mean"]).tail(1))
 
 
+# calculate projections for today's players
+preds_today = xg_reg.predict(df_preds)
+
 # add projections to aggreagted data
-df_today['XGB'] = preds
+df_today['XGB'] = preds_today
 df_today['XGB'] = df_today['XGB'].apply(lambda x: round(x, 2))
 df_today = df_today[['Player', 'Position', 'Team', 'Opponent', 'Salary', 'Date', 'Line',
                      'PP Line', 'Moneyline', 'Over/Under', 'Spread', 'Team Pts', 'FC', 'RW',
