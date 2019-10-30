fe5e42bd0095c8f0c59e39733eeda6b452d7a548
pnacamuli17 <panac@nacamuli.net>
2019-10-30 17:04:28 -0500

    update script

--- nhl/scripts/batch/00 - Run All.bat
+++ nhl/scripts/batch/00 - Run All.bat
@@ -1,11 +1,13 @@
 timeout 5
-CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\01 - Scrape Projections.bat"
+CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\02 - Scrape Projections.bat"
 timeout 5
-CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\02 - Prep Data for Analysis.bat"
+CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\03 - Prep Data for Analysis.bat"
+timeout 5
+CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\04 - Predictive Models.bat"
 timeout 5
 CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\05 - Upload Projections to FC.bat"
 timeout 5
-CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\06 - Archive and Append Timestamps.bat"
+CALL "c:\dev\Python\Repos\dfs-model\nhl\scripts\batch\06 - Archive and Append Dates.bat"
 timeout 5
 exit
 
